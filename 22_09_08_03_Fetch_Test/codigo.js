6eff326680865b506e9131ce9dd0c82ed120215b
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2022-09-08 16:28:59 -0500

    Excercise to Confirm Learned

---
+++ 22_09_08_03_Fetch_Test/codigo.js
@@ -0,0 +1,13 @@
+//** Constante que apunte al container de qS */
+//** Url del EndPoint */
+//** Metodo para recibir informacion, parametro que recibe(de donde trae la info) */
+//** Entonces, el nuevo arreglo, FF, el nuevo arreglo se presenta en formato */
+//** Metodo, nuevo arreglo, FF, instrucciones */
+//** Arreglo, para cada uno, creo un nuevo arreglo, FF e instrucciones */
+//** Nueva variable, en el documento creo un nuevo elemento, y digo cual */
+//** Variable le creo un nuevo metodo y le coloco el atributo id y de donde viene */
+//** Variable, la muestro en el html y que quiero mostrar */
+//** Variable le paso un evento que este atento, cuando haga click, FF anonima */
+//** Direccion  donde se va a redireccionar */
+//** Variable, que aparezca, que quiero que aparezca */
+//** Cacha el error */
---
+++ 22_09_08_03_Fetch_Test/usuario.js
@@ -0,0 +1,11 @@
+//** Creo variable y del documento selecciono el contenedor */
+//** Variable para conseguir la Url, nueva instancia del metodo URLSP y recibe win.loc.ser* */
+//** Solicito el id */
+//** La URL del endPoint */
+//** Metodo para conseguir informacion de una api, pero le concateno el url y el id, pongo una barra en medio */
+//** Entonces, me vas a traer una respuesta, esta variable en que respuesta */
+//** Entonces traime la data */
+//** VN, en el documento voy a crear un nuevo elemento del tipo */
+//** VC, se va a insertar en el HTML pero de donde viene la info */
+//** Donde quiero mostrar lo que saque */    
+//** Cacha el error */
