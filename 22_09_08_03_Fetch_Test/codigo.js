e4aed39d0a60273940b4a1880b658cdf663cd428
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2022-09-10 22:30:12 -0500

    Working

--- 22_09_08_03_Fetch_Test/codigo.js
+++ 22_09_08_03_Fetch_Test/codigo.js
@@ -1,13 +1,19 @@
-//** Constante que apunte al container de qS */
-//** Url del EndPoint */
-//** Metodo para recibir informacion, parametro que recibe(de donde trae la info) */
-//** Entonces, el nuevo arreglo, FF, el nuevo arreglo se presenta en formato */
-//** Metodo, nuevo arreglo, FF, instrucciones */
-//** Arreglo, para cada uno, creo un nuevo arreglo, FF e instrucciones */
-//** Nueva variable, en el documento creo un nuevo elemento, y digo cual */
-//** Variable le creo un nuevo metodo y le coloco el atributo id y de donde viene */
-//** Variable, la muestro en el html y que quiero mostrar */
-//** Variable le paso un evento que este atento, cuando haga click, FF anonima */
-//** Direccion  donde se va a redireccionar */
-//** Variable, que aparezca, que quiero que aparezca */
-//** Cacha el error */
+
+
+const app = document.querySelector('.container')
+const url = `https://jsonplaceholder.typicode.com/users`
+
+fetch(url)
+.then(res => res.json())
+.then(data => {
+    data.forEach(usuario => {
+        const usuarioIndividual = document.createElement('p')
+        usuarioIndividual.setAttribute('id', usuario.id)
+        usuarioIndividual.innerHTML = usuario.name
+        usuarioIndividual.addEventListener('click', () => {
+            window.location.href = `./usuario.html?id=${usuario.id}`
+        })
+        app.appendChild(usuarioIndividual)
+    })
+})
+.catch(err => err)
--- 22_09_08_03_Fetch_Test/usuario.js
+++ 22_09_08_03_Fetch_Test/usuario.js
@@ -1,6 +1,6 @@
 //** Creo variable y del documento selecciono el contenedor */
 //** Variable para conseguir la Url, nueva instancia del metodo URLSP y recibe win.loc.ser* */
-//** Solicito el id */
+//** Variable del id, de la misma variable con el metodo conseguir y elegir el elemento que quiero */
 //** La URL del endPoint */
 //** Metodo para conseguir informacion de una api, pero le concateno el url y el id, pongo una barra en medio */
 //** Entonces, me vas a traer una respuesta, esta variable en que respuesta */
@@ -9,3 +9,18 @@
 //** VC, se va a insertar en el HTML pero de donde viene la info */
 //** Donde quiero mostrar lo que saque */    
 //** Cacha el error */
+
+const app = document.querySelector(".container")
+const getUrl = new URLSearchParams(window.location.search)
+const id = getUrl.get('id')
+const url = `https://jsonplaceholder.typicode.com/users`
+
+fetch(`${url}/${id}`)
+.then(res => res.json())
+.then(data => {
+    const usuarioIndividual = document.createElement('p')
+    usuarioIndividual.innerHTML = data.name
+    app.appendChild(usuarioIndividual)
+    console.log(usuarioIndividual);
+})
+.catch(err => err)
