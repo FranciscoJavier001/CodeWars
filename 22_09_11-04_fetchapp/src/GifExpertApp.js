636cb1a81ac1128eeaadb4c438422c47a4c67c20
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2022-09-18 18:16:07 -0500

    App Working, Map to create new Element Array

--- 22_09_11-04_fetchapp/src/GifExpertApp.js
+++ 22_09_11-04_fetchapp/src/GifExpertApp.js
@@ -5,20 +5,20 @@ import { GifGrid } from './components/GifGrid'
 export const GifExpertApp = () => {
 
     //** uS necesita un estado actual, que el 3, despues ocupa un nuevo estado y mostrar nuevo estado */
-    const [categories, setCategories] = useState(['Death Note'])
+    const [categories, setCategories] = useState(['']) //** Estado, Estado Actualizado, Estado Inicial */
 
   return (
     <>
-    <h2>XXX1</h2>
-    <AddCategory setCategories={setCategories} />
-    <hr/>
+    <h2 id="GEA-h2">Images</h2> {/* Texto, tiene algo de estilos */}
+    <AddCategory setCategories={setCategories} /> {/* Componente que renderizo, con esos parametros actualizados */}
 
-    <ol>
+
+    <ol> {/* Muestra una lista ordenada */}
         {
-            categories.map(category => (
-                <GifGrid
-                key={category}
-                category={category}
+            categories.map(category => ( //** Mapea el categories, con u nuevo arreglo */
+                <GifGrid //** Renderiza este componente */
+                key={category} //** Key=Para elementos de lista, para mantener los estados(propiedades), key es para elementos unicos */
+                category={category} //** Para mostrar la categoria de la cual se hizo la busqueda */
                 />
             ))
         }
