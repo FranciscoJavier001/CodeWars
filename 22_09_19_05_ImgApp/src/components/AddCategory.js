089e6408e12c3e23aeede0e62c2d3c2f372833e7
Francisco Javier Martinez Duran <user.streaming.001@gmail.com>
2022-09-23 22:27:35 -0500

    Updated Files

---
+++ 22_09_19_05_ImgApp/src/components/AddCategory.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import PropTypes from 'prop-types' //** npm install --save prop-types */
+
+export const AddCategory = (props) => {
+    console.log(props);
+  return (
+    <div>AddCategory</div>
+  )
+}
